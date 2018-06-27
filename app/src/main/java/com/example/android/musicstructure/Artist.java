76aa1c15aeb43520cbd8c90ecdef7e6d2f93e5d8
A-Barrett <mymajestic1@hotmail.com>
2018-06-26 23:15:17 -0400

    test

--- app/src/main/java/com/example/android/musicstructure/Artist.java
+++ app/src/main/java/com/example/android/musicstructure/Artist.java
@@ -1,5 +1,6 @@
 package com.example.android.musicstructure;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
