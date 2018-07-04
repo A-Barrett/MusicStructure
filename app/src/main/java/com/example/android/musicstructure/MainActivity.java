c07def57464e6fa2c916dd7f1bb9517446ba496e
A-Barrett <mymajestic1@hotmail.com>
2018-07-04 15:16:34 -0400

    adapter

--- app/src/main/java/com/example/android/musicstructure/MainActivity.java
+++ app/src/main/java/com/example/android/musicstructure/MainActivity.java
@@ -4,48 +4,52 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String KEY_SONG = "KEY_SONG";
+    public static final String KEY_ARTIST = "KEY_ARTIST";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        // Set the content of the activity to use the activity_main.xml layout file
-        setContentView( R.layout.activity_main );
+        ArrayList<String> song = new ArrayList<>();
 
-        // Find the View that shows the songs category
-        TextView song_title = (TextView) findViewById( R.id.song_title );
 
-        // Set a click listener on that View
-        song_title.setOnClickListener( new View.OnClickListener() {
-            // The code in this method will be executed when the songs category is clicked on.
+        song.add("Where Is The Love, Blackeyed Peas");
+        song.add("The Fighter, Keith Urban");
+        song.add("Heaven, What Ifs, Kane Brown");
+        song.add("Tennessee Whiskey, Chris Stapleton");
+        song.add("Highway Don't Care, Don't Take The Girl, Tim Mcgraw");
+        song.add("When I Get Where I'm Going, Brad Paisley");
+        song.add("If Heaven Wasn't So Far Away, Justin Moore");
+        song.add("Where Were You When The World Stopped Turning, Alan Jackson");
+        song.add("Huntin' Fishin' And Lovin' Everyday, Luke Bryan");
+        song.add("Shine On, Florida Georgia Line");
+
+        final SongAdapter adapter = new SongAdapter(this, song);
+
+        ListView listView = findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                // Create a new intent to open the SongsActivity}
-                Intent songsIntent = new Intent( MainActivity.this, Songs.class );
-
-                // Start the new activity
-                startActivity( songsIntent );
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Song item = adapter.getItem(position);
+                Intent nowPlaying = new Intent(getApplicationContext(), NowPlaying.class);
+                nowPlaying.putExtra(KEY_SONG, item.getSong());
+                nowPlaying.putExtra(KEY_ARTIST, item.getArtist());
+                startActivity(nowPlaying);
             }
-        } );
+        });
 
-        // Find the View that shows the artist category
-        TextView song_artist = (TextView) findViewById( R.id.song_artist );
-
-        // Set a click listener on that View
-        song_artist.setOnClickListener( new View.OnClickListener() {
-            // The code in this method will be executed when the artist category is clicked on.
-            @Override
-            public void onClick(View view) {
-                // Create a new intent to open the ArtistActivity}
-                Intent artistIntent = new Intent( MainActivity.this, Artist.class );
-
-                // Start the new activity
-                startActivity( artistIntent );
-            }
-        } );
     }
 }
 
