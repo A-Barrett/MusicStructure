de32ae39da863183fe94ba0765210a8c44ecd4bb
A-Barrett <mymajestic1@hotmail.com>
2018-07-05 20:27:45 -0400

    Revised adapter

--- app/src/main/java/com/example/android/musicstructure/MainActivity.java
+++ app/src/main/java/com/example/android/musicstructure/MainActivity.java
@@ -17,42 +17,39 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.song_list);
 
-        ArrayList<String> song = new ArrayList<>();
+        ArrayList<Song> songs = new ArrayList<Song>();
+        songs.add(new Song("Where Is The Love", "Black Eyed Peas"));
+        songs.add(new Song("What Ifs", "Kane Brown"));
+        songs.add(new Song("The Fighter", "Keith Urban"));
+        songs.add(new Song("Heaven", "Kane Brown"));
+        songs.add(new Song("Tennessee Whiskey", "Chris Stapleton"));
+        songs.add(new Song("Highway Don't Care", "Time Mcgraw"));
+        songs.add(new Song("When I Get Where I'm Going", "Brad Paisley"));
+        songs.add(new Song("If Heaven Wasn't so Far Away", "Justin Moore"));
+        songs.add(new Song("Shine On", "Florida Georgia Line"));
+        songs.add(new Song("Don't Take The Girl", "Tim Mcgraw"));
+        songs.add(new Song("Huntin' Fishin' And Lovin' Everyday", "Luke Bryan"));
 
+        final SongAdapter adapter = new SongAdapter(this, songs);
 
-        song.add("Where Is The Love, Blackeyed Peas");
-        song.add("The Fighter, Keith Urban");
-        song.add("Heaven, What Ifs, Kane Brown");
-        song.add("Tennessee Whiskey, Chris Stapleton");
-        song.add("Highway Don't Care, Don't Take The Girl, Tim Mcgraw");
-        song.add("When I Get Where I'm Going, Brad Paisley");
-        song.add("If Heaven Wasn't So Far Away, Justin Moore");
-        song.add("Where Were You When The World Stopped Turning, Alan Jackson");
-        song.add("Huntin' Fishin' And Lovin' Everyday, Luke Bryan");
-        song.add("Shine On, Florida Georgia Line");
-
-        final SongAdapter adapter = new SongAdapter(this, song);
-
-        ListView listView = findViewById(R.id.list);
+        ListView listView = (ListView) findViewById(R.id.list);
 
         listView.setAdapter(adapter);
 
+        // When listView item is clicked, item data is sent to new activity
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Song item = adapter.getItem(position);
+                Song item = (Song) adapter.getItem(position);
                 Intent nowPlaying = new Intent(getApplicationContext(), NowPlaying.class);
                 nowPlaying.putExtra(KEY_SONG, item.getSong());
                 nowPlaying.putExtra(KEY_ARTIST, item.getArtist());
                 startActivity(nowPlaying);
             }
         });
-
     }
 }
-
-
 
 
